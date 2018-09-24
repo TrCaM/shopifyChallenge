c2a84e575a97a5db9bf91c828f901340161b8730
TrCaM <cmthscb@gmail.com>
2018-09-24 01:23:04 -0400

    update to use heroku postgre

--- config/config.js
+++ config/config.js
@@ -10,7 +10,7 @@ module.exports = {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    host: process.env.DB_HOSTNAME,
-    dialect: 'mysql'
+    host: process.env.DATABASE_URL,
+    dialect: 'postgre'
   }
 };
